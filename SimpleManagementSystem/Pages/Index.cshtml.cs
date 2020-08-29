3f15b08f174dacfaa50c6d0e375633a7c3ce8cb9
Dad  S Wonkulah Jr <dadwonkulahjr96@gmail.com>
2020-08-29 11:53:55 +0000

    Add project files.

---
+++ SimpleManagementSystem/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleManagementSystem.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public IndexModel()
+        {
+           
+        }
+
+        public void OnGet()
+        {
+            Message = "The time check in the city of monrovia is " + DateTime.Now.ToLongTimeString();
+        }
+    }
+}
